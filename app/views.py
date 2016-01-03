ece12cdcf2a29af1df9f42b418b3f48a76ae9e22
Darrel <ddayy.guerrero@hotmail.com>
2016-01-02 22:25:23 -0500

    Add template rendering, defined template files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"nickname": 'Darrel'}
+    posts = [
+        {
+            'author': {'nickname': 'Master Chief'},
+            'body': 'Beautiful day in Seattle!'
+        },
+        {
+            'author': {'nickname': 'Agent Locker'},
+            'body': 'All hail the conquering hero!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
