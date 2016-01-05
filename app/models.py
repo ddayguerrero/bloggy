8f2a8c4c93167eaa023dfc281def22eaebf64907
Darrel <ddayy.guerrero@hotmail.com>
2016-01-05 13:16:44 -0500

    Cleanup, handle login view

--- app/models.py
+++ app/models.py
@@ -5,6 +5,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return True
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(str.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, g, url_for, session
+from app import app, lm, oid
 from .forms import LoginForm
+from .models import User
 
 
 @app.route('/')
@@ -24,14 +25,21 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    # Redirect to index if user exists and not authenticated
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
