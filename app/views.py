43c11a04e7b67318cb7e32993017cc4ec673db5b
Darrel <ddayy.guerrero@hotmail.com>
2016-01-05 13:46:40 -0500

    Finish login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, g, url_for, session
-from app import app, lm, oid
+from flask import render_template, flash, redirect, g, url_for, session, request
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, lm, oid, db
 from .forms import LoginForm
 from .models import User
 
@@ -38,6 +39,32 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
 
 @lm.user_loader
